7a250dae1de9151f74fc6f8e29685389c1cbe73f
LucaDantas <luca100sdantas2@gmail.com>
2021-02-13 17:02:05 -0300

    Added Convex Hull Trick

---
+++ Geometry/ConvexHullTrick.cpp
@@ -0,0 +1,29 @@
+// O(n) Convex Hull Trick with deque
+// Insertion and queries with monotonic values (inc, dec)
+
+#include<deque>
+
+struct Line
+{
+	long long a, b; // ax + b
+	Line(long long _a, long long _b) : a(_a), b(_b) {}
+	Line() {}
+	long long val(long long x) { return a*x + b; }
+	long double intersect(Line L) { return (long double)(L.b-b)/(a-L.a); }
+};
+
+struct CHT
+{
+	std::deque<Line> hull;
+	void insert(Line L) {
+		while(hull.size() >= 2 && L.intersect(hull.back())
+			>= L.intersect(hull[(int)hull.size()-2]))
+			hull.pop_back();
+		hull.push_back(L);
+	}
+	Line query(long long x) {
+		while(hull.size() >= 2 && hull[0].val(x) <= hull[1].val(x))
+			hull.pop_front();
+		return hull.front();
+	}
+} cht;
